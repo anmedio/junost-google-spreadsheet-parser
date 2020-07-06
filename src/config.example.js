e025ef3c57a2dc3631cb530a3908a058cf6fccc4
nikolasmelui <slak@samaradom.ru>
2020-07-06 18:07:48 +0400

    Create new config structure

--- src/config.example.js
+++ src/config.example.js
@@ -1,5 +1,11 @@
 // const config = {
-// spreadsheetID: 'your-spreadsheet-id',
+//   spreadsheets: {
+//     tags: 'tags-shreapsheet-id',
+//     frontend: 'frontend-shreapsheet-id',
+//     nodejs: 'nodejs-shreapsheet-id',
+//     python: 'python-shreapsheet-id',
+//     java: 'java-shreapsheet-id',
+//   },
 // };
 
 // module.exports = config;

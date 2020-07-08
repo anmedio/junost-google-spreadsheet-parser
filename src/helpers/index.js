ebd4c3196b5403118b70ec1cf42412b1b9bc73a0
nikolasmelui <slak@samaradom.ru>
2020-07-08 10:50:42 +0400

    Create codegen method generateGetSkillsFromFSMethod (much imports)

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -5,8 +5,10 @@ const path = require('path');
 const fs = require('fs').promises;
 const { GoogleSpreadsheet } = require('google-spreadsheet');
 
+const getSkillsFromFS = require('./getSkillsFromFS');
+
 const saveData = async (data, filename) => {
-  const dataPath = path.resolve(__dirname, 'data', filename);
+  const dataPath = path.resolve(__dirname, '..', 'data', filename);
   await fs.writeFile(dataPath, data);
 };
 
@@ -17,4 +19,40 @@ const loadDocument = async (id, credentials) => {
   return document;
 };
 
-module.exports = { saveData, loadDocument };
+/**
+ * Metaprogrammins is awesome!!!(no)
+ * Codegenerator method to generate the getSkillsFromFS method
+ */
+// const generateGetSkillsFromFSMethod = async () => {
+//   try {
+//     const skillsPath = path.resolve(__dirname, '..', 'data', 'skills');
+//     const files = await fs.readdir(skillsPath);
+
+//     const mainBuffer = [];
+//     mainBuffer.push(`'use strict';\n`);
+
+//     const arrayBuffer = [];
+//     arrayBuffer.push(`\n\nconst skills = [`);
+
+//     for (const file of files) {
+//       const filename = file.split('.')[0];
+//       const importString = `\nconst ${filename} = require('../data/skills/${file}');`;
+//       mainBuffer.push(importString);
+//       arrayBuffer.push(`${filename}, `);
+//     }
+//     const targetFilePath = path.resolve(__dirname, 'getSkillsFromFS.js');
+//     arrayBuffer.push(`];`);
+//     const foreachBuffer = `\n\nconst getSkillsFromFS = () => skills;\n\nmodule.exports = getSkillsFromFS;`;
+//     const fileData = mainBuffer.concat(arrayBuffer, foreachBuffer);
+
+//     await fs.writeFile(targetFilePath, fileData.join(''));
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
+
+module.exports = {
+  saveData,
+  loadDocument,
+  getSkillsFromFS,
+};
